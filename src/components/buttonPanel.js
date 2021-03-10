1af8e4f2ba37e2838000288ff02cca6a8884f284
mimipeshy <perisndanu@gmail.com>
2021-03-11 01:12:50 +0300

    styling button complete

--- src/components/buttonPanel.js
+++ src/components/buttonPanel.js
@@ -3,31 +3,31 @@ import Button from './button';
 
 const ButtonPanel = () => (
   <div className="buttons">
-    <div>
+    <div className="button-style">
       <Button name="AC" />
       <Button name="+/-" />
       <Button name="%" />
       <Button name="÷" />
     </div>
-    <div>
+    <div className="button-style">
       <Button name="7" />
       <Button name="8" />
       <Button name="9" />
       <Button name="X" />
     </div>
-    <div>
+    <div className="button-style">
       <Button name="4" />
       <Button name="5" />
       <Button name="6" />
       <Button name="-" />
     </div>
-    <div>
+    <div className="button-style">
       <Button name="1" />
       <Button name="2" />
       <Button name="3" />
       <Button name="+" />
     </div>
-    <div>
+    <div className="button-style">
       <Button name="0" />
       <Button name="." />
       <Button name="=" />
