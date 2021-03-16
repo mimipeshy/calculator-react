a5d13a463d05287684993f649935cfb4dedf39eb
mimipeshy <perisndanu@gmail.com>
2021-03-16 19:40:48 +0300

    Complete handleclick in app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
 import Display from './display';
 import ButtonPanel from './buttonPanel';
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="app">
-      <>
-        <Display />
-        <ButtonPanel />
-      </>
-    </div>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 
-  );
+  handleClick=buttonName => {
+    this.setState(state => calculate(state, buttonName));
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <>
+          <Display result={this.state.next ? this.state.next : this.state.total} />
+          <ButtonPanel clickHandler={this.handleClick} />
+        </>
+      </div>
+
+    );
+  }
 }
-calculate();
-
-export default App;
