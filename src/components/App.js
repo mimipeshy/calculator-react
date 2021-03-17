fe6ddab9be268f8b29e5855bac5bf131f435d68f
mimipeshy <perisndanu@gmail.com>
2021-03-17 22:36:20 +0300

    refactor app with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,28 @@
 /* eslint-disable react/no-unused-state */
 /* eslint-disable react/destructuring-assignment */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Display from './display';
 import ButtonPanel from './buttonPanel';
 import calculate from '../logic/calculate';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const App = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const handleClick = buttonName => {
+    setState(state => calculate(state, buttonName));
+  };
+  return (
+    <div className="app">
+      <>
+        <Display result={state.next ? state.next : state.total} />
+        <ButtonPanel clickHandler={handleClick} />
+      </>
+    </div>
 
-  handleClick=buttonName => {
-    this.setState(state => calculate(state, buttonName));
-  }
+  );
+};
 
-  render() {
-    return (
-      <div className="app">
-        <>
-          <Display result={this.state.next ? this.state.next : this.state.total} />
-          <ButtonPanel clickHandler={this.handleClick} />
-        </>
-      </div>
-
-    );
-  }
-}
+export default App;
